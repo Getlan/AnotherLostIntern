17358c28f08de2aea087497cc1c102df0b5bf9ff
Getlan <annelyse.nugue@gmail.com>
2019-01-09 19:11:44 +0100

    Click on observable objects

--- Assets/Scripts/ObjectsVersion2/ComplexObservable.cs
+++ Assets/Scripts/ObjectsVersion2/ComplexObservable.cs
@@ -7,8 +7,26 @@ public class ComplexObservable : Observable
     [SerializeField] private GameObject zoneToClick;
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+        if (isInteracting && Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit, 10f))
+            {
+                if(hit.transform != null && hit.transform.gameObject == zoneToClick)
+                {
+                    ActivateComplexZone();
+                }
+            }
+        }
+    }
+
+    private void ActivateComplexZone()
+    {
+        iTween.ShakePosition(this.gameObject,new Vector3(0.01f, 0.01f, 0.01f),0.6f);
     }
 }
