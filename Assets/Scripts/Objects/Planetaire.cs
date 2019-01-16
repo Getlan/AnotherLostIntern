8d71be2ea1b8eb847a2f2a0ae295bac3014dd2cb
Getlan <annelyse.nugue@gmail.com>
2019-01-16 13:06:58 +0100

    planétaire fini

--- Assets/Scripts/Objects/Planetaire.cs
+++ Assets/Scripts/Objects/Planetaire.cs
@@ -7,6 +7,7 @@ public class Planetaire : ComplexObservable
 
     private Animator animator;
     private int currentStep = 1;
+    bool canRotate = true;
 
     protected override void Start()
     {
@@ -16,11 +17,24 @@ public class Planetaire : ComplexObservable
 
     protected override void ActivateComplexZone()
     {
-        animator.Play("Step0"+currentStep.ToString());
-        currentStep++;
-        if (currentStep > 6)
+        if (canRotate)
         {
-            currentStep = 1;
+            canRotate = false;
+            animator.Play("Step0" + currentStep.ToString());
+            currentStep++;
+            if (currentStep > 6)
+            {
+                currentStep = 1;
+            }
+            StartCoroutine(WaitAnimationEnd());
         }
     }
+
+    IEnumerator WaitAnimationEnd()
+    {
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        canRotate = true;
+    }
+
+ 
 }
