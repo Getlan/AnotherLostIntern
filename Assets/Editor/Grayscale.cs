8cd6b9d073cdd7fc8787ba842f93ae59edafa7fe
LeandreRenouard <leandre.renouard@gmail.com>
2019-01-17 14:00:23 +0100

    Shader Fix + Mirror material
    le shader reste en place.
    Le material miroir est mis en place

---
+++ Assets/Editor/Grayscale.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Rendering.PostProcessing;
+using UnityEditor.Rendering.PostProcessing;
+
+[PostProcessEditor(typeof(Grayscale))]
+public sealed class GrayscaleEditor : PostProcessEffectEditor<Grayscale>
+{
+    SerializedParameterOverride m_Blend;
+    SerializedParameterOverride m_Radius;
+
+    public override void OnEnable()
+    {
+        m_Blend = FindParameterOverride(x => x.blend);
+        m_Radius = FindParameterOverride(x => x.radius);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        PropertyField(m_Blend);
+        PropertyField(m_Radius);
+    }
+}
