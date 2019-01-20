3deec6b0ebd1edfc8d5b39a3d8c6f1d2f9174026
Getlan <annelyse.nugue@gmail.com>
2019-01-20 04:38:34 +0100

    bible

---
+++ Assets/Scripts/Objects/Specific/BibleProphetie.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BibleProphetie : ComplexObservable
+{
+    private Animator animator;
+    bool open = false;
+    private Collider objectCollider;
+
+    protected override void Start()
+    {
+        base.Start();
+        animator = this.GetComponent<Animator>();
+    }
+
+    protected override void ActivateComplexZone()
+    {
+        if (open)
+        {
+            animator.SetTrigger("close");
+            open = false;
+        }
+        else
+        {
+            animator.SetTrigger("open");
+            open = true;
+        }
+
+    }
+
+    public override void StopInteract()
+    {
+        if (open)
+        {
+            animator.SetTrigger("close");
+            open = false;
+            StartCoroutine(WaitAnimationEnd());
+        }
+        else
+        {
+            FinishStopInteract();
+        }
+    }
+
+    private void FinishStopInteract()
+    {
+        base.StopInteract();
+
+    }
+
+    IEnumerator WaitAnimationEnd()
+    {
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        FinishStopInteract();
+    }
+}
