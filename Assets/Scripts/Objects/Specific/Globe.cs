206a796e41184b0b27a3200732eeef256d5bcad0
Getlan <annelyse.nugue@gmail.com>
2019-01-20 21:16:58 +0100

    Test pour Léandre

--- Assets/Scripts/Objects/Specific/Globe.cs
+++ Assets/Scripts/Objects/Specific/Globe.cs
@@ -7,23 +7,43 @@ public class Globe : Interactive
     [SerializeField] protected Vector3 state1, state2, state3;
     [SerializeField] protected float animationTime;
     private Animator animator;
-    //protected Hashtable iTweenArgs;
+    bool rotate = true;
 
     private void Start()
     {
-        interactCaptionText = "Faire tourner le globe";
         animator = GetComponent<Animator>();
     }
 
     public override void Interact()
     {
-        base.Interact();
-        animator.SetTrigger("rotate");
-        Invoke("StopInteract", animator.GetCurrentAnimatorStateInfo(0).length);
+        if (rotate)
+        {
+            base.Interact();
+            rotate = false;
+            animator.SetTrigger("rotate");
+            StartCoroutine(WaitAnimationEnd());
+        }
+
     }
 
     public override void StopInteract()
     {
         base.StopInteract();
+    }
+
+    public override void IsLooking()
+    {
+        base.IsLooking();
+        if (!IsInteracting)
+        {
+            UIManager.instance.ChangeTextInteractCaption("Faire tourner le globe");
+        }
+    }
+
+    IEnumerator WaitAnimationEnd()
+    {
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length +0.7f);
+        rotate = true;
+        StopInteract();
     }
 }
