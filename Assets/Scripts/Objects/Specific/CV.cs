211f7aceecf7e6c2045010e0fa1e4caff6ac709e
Getlan <annelyse.nugue@gmail.com>
2019-01-23 13:31:21 +0100

    push

---
+++ Assets/Scripts/Objects/Specific/CV.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CV : Observable
+{
+    private TextObject textObject;
+
+    protected override void Start()
+    {
+        base.Start();
+        textObject = this.GetComponent<TextObject>();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        textObject.ChangeTextToRead("CV");
+    }
+
+    public override void StopInteract()
+    {
+        base.StopInteract();
+        textObject.ChangeTextToRead("");
+    }
+}
