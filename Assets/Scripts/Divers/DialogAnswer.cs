30fea48b59ae608a56332e6d8ee8ad8033d513b1
Getlan <annelyse.nugue@gmail.com>
2019-01-31 22:01:39 +0100

    Revert "Revert "interference fix""
    This reverts commit ab57d40114dde5d2a80ead4cc4c799ad35636b49.

---
+++ Assets/Scripts/Divers/DialogAnswer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogAnswer : MonoBehaviour
+{
+    public void Click()
+    {
+        this.gameObject.SetActive(false);
+        AudioManager.instance.Play("Interference");
+    }
+}
